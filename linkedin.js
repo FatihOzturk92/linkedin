8cefc39b7ea0221b9ad6100573dce5bd063ea18e
fatih <my.fatih.92@gmail.com>
2017-11-21 14:21:29 +0300

    Add files via upload

---
+++ linkedin.js
@@ -0,0 +1,17 @@
+var i = 1;
+setInterval(function(){ i++;$('html').scrollTop(250*i); },1000);
+setInterval(function(){ 
+
+
+    $('.mn-pymk-list__card').each(function(index, value) {
+    var text = $(this).find('.mn-person-info__occupation')[0].innerHTML.toLowerCase();
+         if(text.indexOf('developer')>0 || text.indexOf('software')>0 || text.indexOf('yazýlým')>0 )   {
+            setTimeout(function() {
+             $(value).find('.button-secondary-small')[0].click();
+            console.log("eklendi " + index);
+            }, index * 100);
+         }
+        
+     });
+}, 7000);
+//https://www.linkedin.com/mynetwork/
