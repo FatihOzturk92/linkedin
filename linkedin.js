c849438fc1e27b37f5fe13de1d022b80ed47db80
fatih <my.fatih.92@gmail.com>
2018-06-25 16:05:16 +0300

    Update linkedin.js

--- linkedin.js
+++ linkedin.js
@@ -1,6 +1,6 @@
 var i = 1;
 var personelCount = 0;
-setInterval(function () { i++; $('html').scrollTop(250 * i); }, 1000);
+setInterval(function () { i++;  $('html').scrollTop(10);$('html').scrollTop(100000);}, 300);
 setInterval(function () {
 
     var count = $('.mn-pymk-list__card').length;
@@ -20,14 +20,13 @@ setInterval(function () {
     $('.mn-pymk-list__card').each(function (index, value) {
         var text = $(this).find('.pymk-card__occupation')[0].innerHTML.toLowerCase();
 
-        if (text.indexOf('insan') > 0 || text.indexOf('kaynak') > 0 || text.indexOf('uzman') > 0 || text.indexOf('Mühendi') > 0 || text.indexOf('sorum') > 0 || text.indexOf('bilge') > 0)  // job ?
+        if (text.indexOf('insan') > 0 || text.indexOf('kaynak') > 0 || text.indexOf('uzman') > 0 || text.indexOf('Mühendi') > 0  || text.indexOf('sorum') > 0 || text.indexOf('bilge') > 0 || text.indexOf('müdür') > 0|| text.indexOf('öğrenci') > 0|| text.indexOf('irket') > 0|| text.indexOf('üniversite') > 0|| text.indexOf('bilişim') > 0)  // job ?
         {
             setTimeout(function () {
                 $(value).find('.button-secondary-small')[0].click();
-                console.log("eklendi " + personelCount++ + " -- " + text);
+                console.log("eklendi " + personelCount++ + " -- "+text);
             }, index * 100);
         }
 
     });
 }, 7000);
-//https://www.linkedin.com/mynetwork/
